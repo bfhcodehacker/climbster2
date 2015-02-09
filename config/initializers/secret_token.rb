89a510e5bbbc72d2055ca9fb4c1114eebdcd8f59
Brad Habenicht <brad.habenicht@gmail.com>
2015-02-09 01:17:53 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Climbster2::Application.config.secret_key_base = 'b4b46fbbf1b1761bef48e53361b9e2f0473f107ff350fc5fa09f22eabff6c29362c8303958ea9c879f3be6835a84edd58955addc533272df8d394a0ba80db224'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Climbster2::Application.config.session_store :cookie_store, key: '_climbster2_session'
