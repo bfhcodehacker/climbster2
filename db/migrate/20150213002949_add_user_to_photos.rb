a93c4900449ea9e69810b2f28fd8ffca9a96b5a7
Brad Habenicht <brad.habenicht@gmail.com>
2015-02-12 21:21:45 +0000

    added userid column to photos and updated models

---
+++ db/migrate/20150213002949_add_user_to_photos.rb
@@ -0,0 +1,7 @@
+class AddUserToPhotos < ActiveRecord::Migration
+  def change
+    add_column :photos, :user_id, :integer
+    add_index :photos, [:user_id, :place_id]
+    add_index :photos, :place_id
+  end
+end
