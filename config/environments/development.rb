f96febcbe0840e007e3b783ff060521b5a4e81a2
Brad Habenicht <brad.habenicht@gmail.com>
2015-02-13 22:36:34 +0000

    automated email notification for comments

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -5,17 +5,17 @@ Climbster2::Application.configure do
   config.action_mailer.default_url_options = { :host => 'localhost:3030' }
 
   # sep up mailer to work with gmail
-  config.action_mailer.delivery_method = :smtp
+  #  config.action_mailer.delivery_method = :smtp
 
-  config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
-    port: 587,
-    domain: "example.com",
-    authentication: "plain",
-    enable_starttls_auto: true,
-    user_name: ENV['GMAIL_ADDRESS'],
-    password: ENV['GMAIL_PASSWORD']
-  }
+  #  config.action_mailer.smtp_settings = {
+  #    address: "smtp.gmail.com",
+  #    port: 587,
+  #    domain: "example.com",
+  #    authentication: "plain",
+  #    enable_starttls_auto: true,
+  #    user_name: ENV['GMAIL_ADDRESS'],
+  #    password: ENV['GMAIL_PASSWORD']
+  #  }
 
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
